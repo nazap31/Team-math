404788c5904634b24fa084f8cad61945c79a9b68
nazap31 <you@example.com>
2022-06-08 13:00:59 -0600

    team math

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Participantes
+// Jose Ramirez
+// Dilan Fuentes
+// nazareth bonilla
+// Gersón Portocarrero
+// adley marchena
+// Alexander Mondragon
+
+// Suma
+Console.WriteLine(15 + 30);
+Console.WriteLine(10 + 14);
+// Resta
+Console.WriteLine(30 - 10);
+Console.WriteLine(10 - 5);
+Console.WriteLine(20 - 7);
+// Multiplicación
+Console.WriteLine(10 * 20);
+Console.WriteLine(27 * 31);
+
+// Division
+Console.WriteLine(Decimal.Divide(5, 2)); // //
+// Exponente
+Console.WriteLine(Math.Pow(3, 2)); // 3 ** 2
+
+// Division entera
+Console.WriteLine(21 / 10);
+
+// Módulo
+Console.WriteLine(28 % 3);
